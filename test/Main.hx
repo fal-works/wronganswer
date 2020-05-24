7fd170e561122f1b6fc0c54fc403b2a280873790
FAL <falworks.contact@gmail.com>
2020-05-24 20:12:48 +0900

    edit tests

--- test/Main.hx
+++ test/Main.hx
@@ -3,47 +3,61 @@ import haxe.Timer;
 import wronganswer.WrongAnswer;
 
 class Main {
-	static function main():Void {
-		outputUnitTests();
+	static function main() {
+		// inputUnitTests();
+		inputSpeed();
+		// outputUnitTests();
 		// outputSpeed();
 	}
 
-	static function testInput():Void {
+	static function inputUnitTests() {
 		// final input = Sys.stdin();
 		// // final a = input.readUntil(" ".code);
 		// final a = input.readLine();
 		// input.close();
 	}
 
-	static function outputUnitTests():Void {
-		println("\n[output unit tests]");
-		final out = new StdOut();
-		out.int(10).space().int(20).lf();
-		out.char("A".code).lf();
-		out.float(1.5).lf();
-		var int64Value: Int64 = 0;
-		for (i in 0...1000) int64Value += 1000000000;
-		out.int64(int64Value).lf();
-		out.flush();
-	}
-
-	static function outputSpeed():Void {
-		println("\n[output speed]");
+	static function inputSpeed() {
+		Sys.println("\n[input speed]");
 		Timer.measure(() -> {
-			final out = new StdOut();
-			out.int(777);
-			for (i in 0...10000) {
-				out.int(i).lf();
+			final stdin = Sys.stdin();
+			final count = Std.parseInt(stdin.readLine());
+			for (i in 0...count - 1) {
+				stdin.readUntil(" ".code);
+				stdin.readUntil("\n".code);
 			}
-			out.flush();
+			stdin.readUntil(" ".code);
+			final lastNumber = stdin.readLine();
+			trace("last number: " + lastNumber);
 		});
 	}
 
-	static function println(s:String): Void {
-		#if js
-		(untyped process).stdout.write(s + "\n");
-		#else
-		Sys.println(s);
-		#end
+	static function outputUnitTests() {
+		Sys.println("\n[output unit tests]");
+		final stdout = new StdOut();
+		stdout.int(10).space().int(20).lf();
+		stdout.char("A".code).lf();
+		stdout.float(1.5).lf();
+		var int64Value:Int64 = 0;
+		for (i in 0...1000)
+			int64Value += 1000000000;
+		stdout.int64(int64Value).lf();
+		stdout.flush();
+	}
+
+	static function outputSpeed() {
+		Sys.println("\n[output speed]");
+		Timer.measure(() -> {
+			final stdout = new StdOut();
+			stdout.int(10000).lf();
+			var i = 0;
+			while (i < 20000) {
+				stdout.int(i).space();
+				++i;
+				stdout.int(i).lf();
+				++i;
+			}
+			stdout.flush();
+		});
 	}
 }

54daeed30dccce58f37f111493844fae726f91c1
FAL <falworks.contact@gmail.com>
2020-05-29 23:01:30 +0900

    use Int for Char also on java

--- src/wa/Char.java.hx
+++ src/wa/Char.java.hx
@@ -1,12 +1,6 @@
 package wa;
 
-/**
-	Character code.
-**/
-abstract Char(#if macro Int #else java.types.Int8 #end) from Int #if !macro to java.types.Int8 #end {
-	/**
-		@return `true` if `characterCode` is not a whitespace (SP, HT, LF or CR).
-	**/
+abstract Char(Int) from Int to Int {
 	public inline function isNotWhiteSpace():Bool {
 		return switch this {
 			case " ".code | "\t".code | "\n".code | "\r".code:
@@ -16,27 +10,12 @@ abstract Char(#if macro Int #else java.types.Int8 #end) from Int #if !macro to j
 		}
 	}
 
-	/**
-		Converts `this` to an decimal integer digit.
-	**/
 	public inline function toDigit():Int
-		return int32() - "0".code;
+		return this - "0".code;
 
-	/**
-		Converts `this` to `String`.
-	**/
 	public inline function toString():String
-		return String.fromCharCode(int32());
+		return String.fromCharCode(this);
 
-	/**
-		Casts `this` to `Int`.
-	**/
-	@:to inline function int32():Int
-		return #if macro this; #else untyped __java__("(int) {0}", this); #end
-
-	/**
-		Casts `this` to 16bit char.
-	**/
 	inline function char16():#if macro Int #else java.types.Char16 #end
 		return #if macro this; #else untyped __java__("(char) {0}", this); #end
 }

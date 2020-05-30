33c8edb703b248eda0b9dd86465f075af8377f16
FAL <falworks.contact@gmail.com>
2020-05-31 03:34:16 +0900

    add CharIns.int64() etc.

--- src/wa/CharIns.hx
+++ src/wa/CharIns.hx
@@ -2,13 +2,42 @@ package wa;
 
 import wa.CharIn;
 import wa.Char32;
-import wa.Floats;
+import wa.Int64;
 import wa.Bits;
+import wa.Floats;
 
 /**
 	Utility static functions for `CharIn`.
 **/
 class CharIns {
+	/**
+		Reads an `Int64` value separated by any whitespace character (SP, HL, CR or LF).
+	**/
+	public static inline function int64(cin:CharIn):Int64 {
+		var result:Int64 = 0;
+		var negative = false;
+		try {
+			var character = cin.char();
+			if (character == '-'.code) {
+				negative = true;
+				character = cin.char();
+			}
+			while (character.isNotWhiteSpace()) {
+				result = 10 * result + character.toDigit();
+				character = cin.char();
+			}
+		} catch (e:haxe.io.Eof) {}
+
+		return if (negative) -result else result;
+	}
+
+	/**
+		Reads an `Int64` value (assuming unsigned)
+		separated by any whitespace character (SP, HL, CR or LF).
+	**/
+	public static inline function uint64(cin:CharIn):Int64
+		return uint64WithRadix(cin, 10);
+
 	/**
 		Reads a binary integer separated by any whitespace character (SP, HL, CR or LF).
 	**/
@@ -42,6 +71,29 @@ class CharIns {
 	}
 
 	/**
+		Reads a sequence of `Int64` values separated by any whitespace characters (SP, HL, CR or LF).
+		@see `strVec()` for notes.
+	**/
+	public static inline function int64Vec(cin:CharIn, length:Int):haxe.ds.Vector<Int64> {
+		final vec = new haxe.ds.Vector<Int64>(length);
+		for (i in 0...length)
+			vec[i] = int64(cin);
+		return vec;
+	}
+
+	/**
+		Reads a sequence of `Int64` values (assuming unsigned)
+		separated by any whitespace characters (SP, HL, CR or LF).
+		@see `strVec()` for notes.
+	**/
+	public static inline function uint64Vec(cin:CharIn, length:Int):haxe.ds.Vector<Int64> {
+		final vec = new haxe.ds.Vector<Int64>(length);
+		for (i in 0...length)
+			vec[i] = uint64(cin);
+		return vec;
+	}
+
+	/**
 		Reads a sequence of `Float` values separated by any whitespace characters (SP, HL, CR or LF).
 		@see `strVec()` for notes.
 	**/
@@ -69,5 +121,26 @@ class CharIns {
 		} catch (e:haxe.io.Eof) {}
 
 		return foundCount;
+	}
+
+	/**
+		Reads an unsigned number and returns as `Int64`.
+		Alphabets must be in lower case.
+	**/
+	static inline function uint64WithRadix(cin:CharIn, radix:Int64):Int64 {
+		var result:Int64 = 0;
+		try {
+			var character:Char32;
+			while ((character = cin.char()).isNotWhiteSpace()) {
+				final digit = character.toDigit();
+				#if debug
+				if (digit < 0 || radix <= digit)
+					throw "Failed to parse.";
+				#end
+				result = radix * result + digit;
+			}
+		} catch (e:haxe.io.Eof) {}
+
+		return result;
 	}
 }
--- src/wa/CharIns.js.hx
+++ src/wa/CharIns.js.hx
@@ -2,10 +2,40 @@ package wa;
 
 import wa.CharIn;
 import wa.Char32;
-import wa.Floats;
+import wa.Int64;
 import wa.Bits;
+import wa.Floats;
 
 class CharIns {
+	public static inline function int64(cin:CharIn):Int64 {
+		@:privateAccess final buffer = cin.buffer();
+		var result:Int64 = 0;
+		var negative = false;
+		#if !macro
+		final readSync = js.node.Fs.readSync;
+		try {
+			readSync(0, buffer, 0, 1, null);
+			var character:Char32 = buffer[0];
+			if (character == "-".code) {
+				negative = true;
+				readSync(0, buffer, 0, 1, null);
+				character = buffer[0];
+			}
+			while (character.isNotWhiteSpace()) {
+				result = 10 * result + character.toDigit();
+				if (readSync(0, buffer, 0, 1, null) == 0)
+					break;
+				character = buffer[0];
+			}
+		} catch (e:Dynamic) {}
+		#end
+
+		return if (negative) -result else result;
+	}
+
+	public static inline function uint64(cin:CharIn):Int64
+		return uint64WithRadix(cin, 10);
+
 	public static inline function bits(cin:CharIn):Bits
 		return @:privateAccess cin.uintWithRadix(2);
 
@@ -39,6 +69,20 @@ class CharIns {
 		return result;
 	}
 
+	public static inline function int64Vec(cin:CharIn, length:Int):haxe.ds.Vector<Int64> {
+		final vec = new haxe.ds.Vector<Int64>(length);
+		for (i in 0...length)
+			vec[i] = int64(cin);
+		return vec;
+	}
+
+	public static inline function uint64Vec(cin:CharIn, length:Int):haxe.ds.Vector<Int64> {
+		final vec = new haxe.ds.Vector<Int64>(length);
+		for (i in 0...length)
+			vec[i] = uint64(cin);
+		return vec;
+	}
+
 	public static inline function floatVec(cin:CharIn, length:Int):haxe.ds.Vector<Float> {
 		final vec = new haxe.ds.Vector<Float>(length);
 		for (i in 0...length)
@@ -64,5 +108,25 @@ class CharIns {
 		#end
 
 		return foundCount;
+	}
+
+	static inline function uint64WithRadix(cin:CharIn, radix:Int64):Int64 {
+		@:privateAccess final buffer = cin.buffer();
+		var result:Int64 = 0;
+		#if !macro
+		final readSync = js.node.Fs.readSync;
+		try {
+			readSync(0, buffer, 0, 1, null);
+			var character:Char32 = buffer[0];
+			while (character.isNotWhiteSpace()) {
+				result = radix * result + character.toDigit();
+				if (readSync(0, buffer, 0, 1, null) == 0)
+					break;
+				character = buffer[0];
+			}
+		} catch (e:Dynamic) {}
+		#end
+
+		return result;
 	}
 }

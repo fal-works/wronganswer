42db3649b414886d090f27fe950dc4e02f574cfe
FAL <falworks.contact@gmail.com>
2020-05-30 19:22:43 +0900

    optimize Ints.abs() on java

--- src/wa/Ints.java.hx
+++ src/wa/Ints.java.hx
@@ -10,6 +10,7 @@ class Ints {
 	@:pure public static inline function div(n:Int, divisor:Int):Int
 		return #if macro 0; #else untyped __java__("{0} / {1}", n, divisor); #end
 
-	@:pure public static inline function abs(n:Int):Int
-		return #if macro 0; #else java.lang.Math.abs(n); #end
+	@:pure public static inline function abs(n:Int):Int {
+		return (n ^ (n >> 31)) - (n >> 31);
+	}
 }

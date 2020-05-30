dae65ba58d00d1427b6529f455fde55b2d6ed75c
FAL <falworks.contact@gmail.com>
2020-05-31 01:12:39 +0900

    add Vecs.sum()/sumIn()

--- src/wa/Vecs.hx
+++ src/wa/Vecs.hx
@@ -33,6 +33,30 @@ class Vecs {
 		return new haxe.ds.Vector<T>(length);
 
 	/**
+		@return The sum of all values in `vec`.
+	**/
+	public static inline function sum(vec:haxe.ds.Vector<Int>):Int {
+		var total = 0;
+		for (i in 0...vec.length)
+			total += vec[i];
+		return total;
+	}
+
+	/**
+		@return The sum of values in `vec` from index `start` until (but not including) `end`.
+	**/
+	public static inline function sumIn(vec:haxe.ds.Vector<Int>, start:Int, end:Int):Int {
+		#if debug
+		if (start < 0 || vec.length < end)
+			throw "Out of bounds.";
+		#end
+		var total = 0;
+		for (i in start...end)
+			total += vec[i];
+		return total;
+	}
+
+	/**
 		Creates a reversed vector. This does not change the original vector.
 	**/
 	@:generic
